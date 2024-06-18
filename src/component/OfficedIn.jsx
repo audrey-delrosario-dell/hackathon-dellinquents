534903004b43fe0f8065bbd12fa28ea24bedbd7a
beaSaviozzi <Beatrice.Saviozzi@dell.com>
2024-06-18 09:10:32 +0100

    new-files

--- src/component/OfficedIn.jsx
+++ src/component/OfficedIn.jsx
@@ -5,6 +5,15 @@ const OfficedIn = () => {
     return (
         <div className='officed-in'>
             <h1>OfficedIn</h1>
+            <button className="toggle-btn">toggle</button>
+            <div className="user-icons">
+                {users.map((user) => (
+                    <div key={user.name} className="user">
+                        <img src={user.profile_pic} alt={user.name} className="user-img"/>
+                    </div>
+                ))}
+            </div>
+            <div className="bar-chart"></div>
         </div>
     );
 };

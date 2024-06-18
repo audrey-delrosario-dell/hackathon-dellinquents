d91051129229256b2bf3b527e5c30d654d911769
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-18 09:46:15 +0100

    Implement Dell CSS

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,20 @@ import App from './App.jsx'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
 
+// supporting Dell Style 
+// React (Required)
+import "@dds/react/css/dds-components.css";
+
+// Foundations (Highly recommended)
+import "@dds/react/css/dds-reboot.css";
+import "@dds/react/css/dds-fonts.css";
+import "@dds/react/css/dds-icons.css";
+
+// Foundations (Optional)
+import "@dds/react/css/dds-main.css";
+import "@dds/react/css/dds-helpers.css";
+import "@dds/react/css/dds-templates.css";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

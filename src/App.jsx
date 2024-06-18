df8af109d2d59171d4878804f1d8de34d89de249
AlannahBennettDell <Alannah.Bennett@dell.com>
2024-06-18 09:44:08 +0100

    User Menu Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import LunchedIn from './component/LunchedIn';
 import MyLinks from "./component/MyLinks";
 import MyStatus from "./component/MyStatus";
 import OfficedIn from "./component/OfficedIn";
+import UserMenu from "./component/UserMenu";
 
 function App() {
 
   return (
     <div>
       <Header/>
+      <UserMenu/>
     </div>
   )
 }

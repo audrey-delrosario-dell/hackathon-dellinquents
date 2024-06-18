0773ccdc298c198b683c8a893c47c2361f6c4c66
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-18 15:43:05 +0100

    Implement Whole Section 1st Draft

--- src/component/MyStatus.jsx
+++ src/component/MyStatus.jsx
@@ -1,13 +1,40 @@
 // Top Right Section 
-
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import LunchSelect from './LunchSelect';
+import {DDSRadioButton, DDSRadioGroup, DDSButton} from "@dds/react";
+import "../assets/styles/MyStatus.css";
+import LunchStatus from './LunchStatus';
+import OfficeStatus from './OfficeStatus';
+import TagRadioBtnGroup from './TagRadioBtnGroup';
+import TagsSection from './TagsSection';
 
 const MyStatus = () => {
+    const radioBtnRef = useRef(null);
+    useEffect(() => {
+        // The console log should return { wrapper, ... }  
+        console.log(radioBtnRef.current); 
+    }, []);
+
+
+
     return (
         <div className='my-status'>
             <h1>My Status</h1>
+            {/* Lunch status section  */}
+            <LunchStatus/>
+
+            {/* Office Section  */}
+            <OfficeStatus/>
+
+            {/* Tags Section  */}
+            <TagsSection/>
+
+
+
         </div>
-    );
+
+
+    ); 
 };
 
 export default MyStatus;

d60db089b648cc5be342c412175a3082996a1a13
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-18 16:20:07 +0100

    Impleneting text centering praise god

--- src/component/MyLinks.jsx
+++ src/component/MyLinks.jsx
@@ -1,11 +1,14 @@
-// Bottom Right 
+import '../assets/styles/MyLinks.css'; 
 
-import React from 'react';
+import { DDSButton } from "@dds/react";
 
 const MyLinks = () => {
     return (
         <div className='my-links'>
             <h1>My Links</h1>
+
+            <DDSButton className="addLink" size="md">Add New Link</DDSButton>
+
         </div>
     );
 };

380b453ee6b5cbc5773da90588b871747cfff56f
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-18 09:46:35 +0100

    Implement Grid Layout

--- src/component/Quadrant.jsx
+++ src/component/Quadrant.jsx
@@ -5,8 +5,31 @@ import OfficedIn from "./OfficedIn";
 
 const Quadrant = () => {
     return (
-        <div>
-            <OfficedIn/>
+        <div className="dds__container-fluid">
+            <div className="dds__row">
+                <div className="dds__col--1 dds__col--sm-3 dds__col--md-6 dds__mb-3">
+                    <div className="dds-sb__sample-box">
+                        <LunchedIn/>
+                    </div>
+                </div>
+                <div className="dds__col--1 dds__col--sm-3 dds__col--md-6 dds__mb-3">
+                    <div className="dds-sb__sample-box">
+                       <MyStatus/> 
+                    </div>
+                </div>
+            </div>
+            <div className="dds__row">
+                <div className="dds__col--1 dds__col--sm-3 dds__col--md-6 dds__mb-3">
+                    <div className="dds-sb__sample-box">
+                        <OfficedIn/>
+                    </div>
+                </div>
+                <div className="dds__col--1 dds__col--sm-3 dds__col--md-6 dds__mb-3">
+                    <div className="dds-sb__sample-box">
+                        <MyLinks/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

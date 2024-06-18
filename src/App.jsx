6a8e3b308babbad55d0455beb34d7d618ceb715f
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-18 11:46:43 +0100

    Streamlining app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import Header from "./component/Header"
-import LunchedIn from './component/LunchedIn';
-import MyLinks from "./component/MyLinks";
-import MyStatus from "./component/MyStatus";
-import OfficedIn from "./component/OfficedIn";
-import Quadrant from "./component/Quadrant";
-import UserMenu from "./component/UserMenu";
+import Header from "./component/Header";
+import Quadrant from './component/Quadrant';
+import UserMenu from './component/UserMenu';
 
-function App() {
 
-  return (
-    <div>
-      <Header/>
-      <UserMenu/>
-      <Quadrant/>
-    </div>
-  )
-}
 
-export default App
+const App = () => {
+    return (
+        <div>
+          <Header/>
+          <UserMenu/>
+          <Quadrant/>
+        </div>
+    );
+};
+
+export default App;

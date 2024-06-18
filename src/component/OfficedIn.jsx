c61d2c63ff477d7345cf3d79635c15d00373e0a7
beaSaviozzi <Beatrice.Saviozzi@dell.com>
2024-06-18 10:57:52 +0100

    switch

--- src/component/OfficedIn.jsx
+++ src/component/OfficedIn.jsx
@@ -1,13 +1,22 @@
 // Bottom Right Section
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import placeholder from '../assets/images/user-ph.png';
 import '../assets/styles/OfficedIn.css';
+import { DDSSwitch } from "@dds/react";
 
 const OfficedIn = () => {
+
+    const switchRef = useRef(true);
+    useEffect(() => { 
+        console.log(switchRef.current); 
+    }, []);
+
     return (
         <div className='officed-in'>
-            <h1>OfficedIn</h1>
-            <button className="toggle-btn">toggle</button>
+            <div className="title">
+                <h1>OfficedIn</h1>
+                <DDSSwitch ref={switchRef} />
+            </div>
             <div className="user-icons">
                 <div className="user">
                     <img src={placeholder}></img>

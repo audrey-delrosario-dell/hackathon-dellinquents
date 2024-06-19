a1b2ee8d9385d5b17e05a12aff683e58f8fe4366
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-19 09:49:13 +0100

    Implement stylistic changes

--- src/component/TagRadioBtnGroup.jsx
+++ src/component/TagRadioBtnGroup.jsx
@@ -1,24 +1,15 @@
 import { DDSRadioButton, DDSRadioGroup  } from "@dds/react";
+import '../assets/styles/TagRadioBtnGroup.css'; 
 
 const TagRadioBtnGroup = () => {
     return (
         <div>
-            <DDSRadioGroup legend="Up for a">
-                <div className='row'>
-                    <div className='col'>
-                        <DDSRadioButton value="chat" label="Chat" size="sm" />
-                        <DDSRadioButton value="coffeebreak" label="Coffee Break" size="sm" />
-                    </div>
-                    <div className='col'>
-                        <DDSRadioButton value="snack" label="Snack Break" size="sm" />
-                        <DDSRadioButton value="game" label="Game/Pool" size="sm" />
-
-                    </div>
-                    <div className='col'>
-                        <DDSRadioButton value="game" label="Work Buddy" size="sm" />
-
-                    </div>
-                </div>
+            <DDSRadioGroup className="btnGroup">
+                <DDSRadioButton name="radio" value="chat" label="Chat" size="sm" />
+                <DDSRadioButton name="radio" value="coffeebreak" label="Coffee Break" size="sm" />
+                <DDSRadioButton name="radio" value="snack" label="Snack Break" size="sm" />
+                <DDSRadioButton name="radio" value="game" label="Game/Pool" size="sm" />
+                <DDSRadioButton name="radio" value="game" label="Work Buddy" size="sm" />
             </DDSRadioGroup>
         </div>
     );

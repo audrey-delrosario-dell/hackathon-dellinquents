53cf031c8c6a4fefaae01b61bdb11e5f5c7d4a0d
audrey-delrosario-dell <audrey.delrosario@dell.com>
2024-06-19 13:37:47 +0100

    test

--- src/component/TagRadioBtnGroup.jsx
+++ src/component/TagRadioBtnGroup.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/TagRadioBtnGroup.css';
 const TagRadioBtnGroup = () => {
     return (
         <div>
-            <DDSRadioGroup className="btnGroup">
+            <DDSRadioGroup legend="" className="btnGroup">
                 <DDSRadioButton name="radio" value="chat" label="Chat" size="sm" />
                 <DDSRadioButton name="radio" value="coffeebreak" label="Coffee Break" size="sm" />
                 <DDSRadioButton name="radio" value="snack" label="Snack Break" size="sm" />
